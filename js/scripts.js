8dda6267af9d9535a45ee6edf92ad27159da7e53
Yvonne Peng & Jennifer Morkunas <student@epicodus.com>
2015-09-23 14:33:10 -0700

    Fix bugs for display text issue, it need space before class '.'

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var playerArray = [];
+
 function Player(mark) {
   this.mark = mark;
 }
@@ -25,8 +27,22 @@ Space.prototype.markBy = function(player) {
   this.markedBy = player;
 }
 
-function currentPlayer(player){
+function initGrid() {
+  for(var i = 1; i <=3; i++){
+    $("#grid").append("<tr>");
+    for(var j = 1; j<=3; j++){
+      $("#grid").append("<td id='"+ i + "_" + j + "'><p><span class='mark'></span></p></td>");
+    }
+    $("#grid").append("</tr>");
+  }
+}
 
+function toggleMark(currentMark) {
+  if(currentMark === playerArray[0].mark){
+    currentMark = playerArray[1].mark;
+  } else {
+    currentMark = playerArray[0].mark;
+  }
 }
 
 $(document).ready(function() {
@@ -37,24 +53,39 @@ $(document).ready(function() {
   $("#mark_X").click(function() {
     player1 = new Player("X");
     player2 = new Player("O");
+    playerArray.push(player1);
+    playerArray.push(player2);
     $(".chooseMark").hide();
     currentMark = player1.mark;
+    console.log(currentMark);
   });
 
 
   $("#mark_O").click(function() {
     player1 = new Player("O");
     player2 = new Player("X");
+    playerArray.push(player1);
+    playerArray.push(player2);
     $(".chooseMark").hide();
     currentMark = player1.mark;
+    console.log(currentMark);
   });
 
+  initGrid();
 
-  $("td#1_1").click(function(){
+
+  $("#1_1").on( "click", function() {
+    // console.log($(this));
     $("#1_1 .mark").text(currentMark);
-    currentMark = player2.mark;
+    toggleMark(currentMark);
   });
 
+
+  $("#2_2").click(function() {
+    $("#2_2 .mark").text(currentMark);
+    toggleMark(currentMark);
+    console.log(currentMark);
+  });
 
 
 }); // end of document
